bcaeb4c59bcb38a9bd5ff051ac952e1cd04b130b
明朝无酒 <51810175+erpcto@users.noreply.github.com>
2024-11-08 21:01:45 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["erpcto.com"];
+pub const PUBLIC_RS_PUB_KEY: &str = "fxP6xF3iDQQo6nasNfCkEi7CE98Wv4hJEXWdhGm0u7E=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
